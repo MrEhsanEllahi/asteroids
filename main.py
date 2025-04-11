2d6b1317555f5fafe57bfa473c7b70dbb5dd2b8c
Ehsan Ellahi <ehsan@Ehsans-MacBook-Pro.local>
2025-04-11 18:36:14 +0500

    draw player on screen

--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame 
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    Clock = pygame.time.Clock()
+    dt = 0 #delta time
+    player  = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #create player object
     while True:
         screen.fill((0,0,0)) # Fill the screen with black
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = Clock.tick(60) / 1000 #time passed since last frame in seconds
 
 if __name__ == "__main__": #if this file is being run directly
     main()
